0290f4aeb7ed02ca21fe7bc16f60f4b82299e31e
li-lauren <laurenlidev@gmail.com>
2021-04-07 20:37:27 -0600

    Add bug resolved

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import store from './store';
-import { bugAdded } from './actions';
+import { bugAdded, bugResolved } from './actions';
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
 store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
 
-unsubscribe();
+// unsubscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1
-  }
-});
+// store.dispatch({
+//   type: actions.BUG_REMOVED,
+//   payload: {
+//     id: 1
+//   }
+// });
 
 console.log(store.getState())
